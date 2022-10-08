f4e8169888a1b19fcfc21e3016e65d6af5a99e73
singh36Pushpendra <psthakur.1999@gmail.com>
2022-10-08 23:36:05 +0530

    Refactored: Refactor2 Using generic class

--- src/programs/generics/FindMaximum.java
+++ src/programs/generics/FindMaximum.java
@@ -1,36 +1,47 @@
 package programs.generics;
 
-public class FindMaximum {
+public class FindMaximum <T extends Comparable<T>> {
 
-	private static <T extends Comparable<T>> T findMaximum(T val1, T val2, T val3) {
-		if (val1.compareTo(val2) > 0 && val1.compareTo(val3) > 0)
-			return val1;
-		else if (val2.compareTo(val3) > 0)
-			return val2;
+	private T value1, value2, value3;
+	
+	public FindMaximum(T value1, T value2, T value3) {
+		this.value1 = value1;
+		this.value2 = value2;
+		this.value3 = value3;
+	}
+
+	private T findMaximum() {
+		if (value1.compareTo(value2) > 0 && value1.compareTo(value3) > 0)
+			return value1;
+		else if (value2.compareTo(value3) > 0)
+			return value2;
 		else
-			return val3;
+			return value3;
 	}
 	
 	public static void main(String[] args) {
 		Integer integer1 = 7;
 		Integer integer2 = -92;
 		Integer integer3 = -79;
-		
-		Integer maxInteger = findMaximum(integer1, integer2, integer3);
+
+		FindMaximum<Integer> findMaxInteger = new FindMaximum<>(integer1, integer2, integer3);
+		Integer maxInteger = findMaxInteger.findMaximum();
 		System.out.println(maxInteger + " is the Maximum!");
 		
 		Float float1 = -7.9f;
 		Float float2 = -8.2f;
 		Float float3 = -7.0f;
 		
-		Float maxFloat = findMaximum(float1, float2, float3);
+		FindMaximum<Float> findMaxFloat = new FindMaximum<Float>(float1, float2, float3);
+		Float maxFloat = findMaxFloat.findMaximum();
 		System.out.println(maxFloat + " is the Maximum!");
 		
 		String fruit1 = "RamFruit";
 		String fruit2 = "Peach";
 		String fruit3 = "SitaFruit";
 
-		String maxString = findMaximum(fruit1, fruit2, fruit3);
+		FindMaximum<String> findMaxString = new FindMaximum<String>(fruit1, fruit2, fruit3);
+		String maxString = findMaxString.findMaximum();
 		System.out.println(maxString + " is the Maximum!");
 	}
 }
