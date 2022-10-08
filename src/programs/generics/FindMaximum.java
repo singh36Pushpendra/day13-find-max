90b21ac85ca89a0cd0213cbb7841374c7e1f8705
singh36Pushpendra <psthakur.1999@gmail.com>
2022-10-08 22:33:38 +0530

    Added: UC2 Find max float

--- src/programs/generics/FindMaximum.java
+++ src/programs/generics/FindMaximum.java
@@ -10,12 +10,30 @@ public class FindMaximum {
 		else
 			return num3;
 	}
+	
+	private static Float findMaximum(Float num1, Float num2, Float num3) {
+		if (num1.compareTo(num2) > 0 && num1.compareTo(num3) > 0)
+			return num1;
+		else if (num2.compareTo(num3) > 0)
+			return num2;
+		else
+			return num3;
+	}
+	
 	public static void main(String[] args) {
 		Integer integer1 = 7;
 		Integer integer2 = -92;
 		Integer integer3 = -79;
 		
-		Integer max = findMaximum(integer1, integer2, integer3);
-		System.out.println(max + " is the Maximum!");
+		Integer maxInteger = findMaximum(integer1, integer2, integer3);
+		System.out.println(maxInteger + " is the Maximum!");
+		
+		Float float1 = -7.9f;
+		Float float2 = -8.2f;
+		Float float3 = -7.0f;
+		
+		Float maxFloat = findMaximum(float1, float2, float3);
+		System.out.println(maxFloat + " is the Maximum!");
+		
 	}
 }
