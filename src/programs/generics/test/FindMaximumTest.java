abbb76237f4fd1418642aab1eda2ad7cd8e93914
singh36Pushpendra <psthakur.1999@gmail.com>
2022-11-06 21:00:27 +0530

    Added: TC2.2 Max float at 2nd pos

---
+++ src/programs/generics/test/FindMaximumTest.java
@@ -0,0 +1,14 @@
+package programs.generics.test;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+import programs.generics.FindMaximum;
+
+public class FindMaximumTest {
+
+	@Test
+	public void findMaximumTest() {
+		Assert.assertEquals(7.9F, FindMaximum.findMaximum(5.2f, 7.9f, 7.44f));
+	}
+}
