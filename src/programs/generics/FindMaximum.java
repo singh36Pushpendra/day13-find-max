78b93019011a3a0fe672cac8a04dbec92ebd4081
singh36Pushpendra <psthakur.1999@gmail.com>
2022-11-07 00:31:12 +0530

    Added: UC5 Generic Method to print max

--- src/programs/generics/FindMaximum.java
+++ src/programs/generics/FindMaximum.java
@@ -6,7 +6,13 @@ public class FindMaximum <T extends Comparable<T>> {
 
 	public T findMaximum(T ...values) {
 		Arrays.sort(values);
-		return values[values.length - 1];
+		T max = values[values.length - 1];
+		printMax(max);
+		return max;
+	}
+	
+	public void printMax(T max) {
+		System.out.println("Maximum = " + max);
 	}
 	
 	public static void main(String[] args) {
@@ -15,23 +21,19 @@ public class FindMaximum <T extends Comparable<T>> {
 		Integer integer3 = -79;
 
 		FindMaximum<Integer> findMaxInteger = new FindMaximum<>();
-		Integer maxInteger = findMaxInteger.findMaximum(integer1, integer2, integer3);
-		System.out.println(maxInteger + " is the Maximum!");
+		findMaxInteger.findMaximum(integer1, integer2, integer3);
 		
 		Float float1 = -7.9f;
 		Float float2 = -8.2f;
 		Float float3 = -7.0f;
 		
 		FindMaximum<Float> findMaxFloat = new FindMaximum<Float>();
-		Float maxFloat = findMaxFloat.findMaximum(float1, float2, float3);
-		System.out.println(maxFloat + " is the Maximum!");
-		
+		findMaxFloat.findMaximum(float1, float2, float3);		
 		String fruit1 = "RamFruit";
 		String fruit2 = "Peach";
 		String fruit3 = "SitaFruit";
 
 		FindMaximum<String> findMaxString = new FindMaximum<String>();
-		String maxString = findMaxString.findMaximum(fruit1, fruit2, fruit3);
-		System.out.println(maxString + " is the Maximum!");
-	}
+		findMaxString.findMaximum(fruit1, fruit2, fruit3);
+		}
 }
