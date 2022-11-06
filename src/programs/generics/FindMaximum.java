e22fee5bc6c627b8e7d36c42e8fffe1e1f93c535
singh36Pushpendra <psthakur.1999@gmail.com>
2022-11-07 00:14:13 +0530

    Added: UC4 Change method to take var-args

--- src/programs/generics/FindMaximum.java
+++ src/programs/generics/FindMaximum.java
@@ -1,22 +1,12 @@
 package programs.generics;
 
+import java.util.Arrays;
+
 public class FindMaximum <T extends Comparable<T>> {
 
-	private T value1, value2, value3;
-	
-	public FindMaximum(T value1, T value2, T value3) {
-		this.value1 = value1;
-		this.value2 = value2;
-		this.value3 = value3;
-	}
-
-	public T findMaximum() {
-		if (value1.compareTo(value2) > 0 && value1.compareTo(value3) > 0)
-			return value1;
-		else if (value2.compareTo(value3) > 0)
-			return value2;
-		else
-			return value3;
+	public T findMaximum(T ...values) {
+		Arrays.sort(values);
+		return values[values.length - 1];
 	}
 	
 	public static void main(String[] args) {
@@ -24,24 +14,24 @@ public class FindMaximum <T extends Comparable<T>> {
 		Integer integer2 = -92;
 		Integer integer3 = -79;
 
-		FindMaximum<Integer> findMaxInteger = new FindMaximum<>(integer1, integer2, integer3);
-		Integer maxInteger = findMaxInteger.findMaximum();
+		FindMaximum<Integer> findMaxInteger = new FindMaximum<>();
+		Integer maxInteger = findMaxInteger.findMaximum(integer1, integer2, integer3);
 		System.out.println(maxInteger + " is the Maximum!");
 		
 		Float float1 = -7.9f;
 		Float float2 = -8.2f;
 		Float float3 = -7.0f;
 		
-		FindMaximum<Float> findMaxFloat = new FindMaximum<Float>(float1, float2, float3);
-		Float maxFloat = findMaxFloat.findMaximum();
+		FindMaximum<Float> findMaxFloat = new FindMaximum<Float>();
+		Float maxFloat = findMaxFloat.findMaximum(float1, float2, float3);
 		System.out.println(maxFloat + " is the Maximum!");
 		
 		String fruit1 = "RamFruit";
 		String fruit2 = "Peach";
 		String fruit3 = "SitaFruit";
 
-		FindMaximum<String> findMaxString = new FindMaximum<String>(fruit1, fruit2, fruit3);
-		String maxString = findMaxString.findMaximum();
+		FindMaximum<String> findMaxString = new FindMaximum<String>();
+		String maxString = findMaxString.findMaximum(fruit1, fruit2, fruit3);
 		System.out.println(maxString + " is the Maximum!");
 	}
 }
--- src/programs/generics/test/FindMaximumTest.java
+++ src/programs/generics/test/FindMaximumTest.java
@@ -1,45 +1,18 @@
 package programs.generics.test;
 
-import java.util.Arrays;
-import java.util.Collection;
-
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 
 import junit.framework.Assert;
 import programs.generics.FindMaximum;
 
-@RunWith(Parameterized.class)
-public class FindMaximumTest<T extends Comparable<T>> {
-
-        T value1, value2, value3, maxValue;
-
-        public FindMaximumTest(T value1, T value2, T value3, T maxValue) {
-                this.value1 = value1;
-                this.value2 = value2;
-                this.value3 = value3;
-                this.maxValue = maxValue;
-        }
+public class FindMaximumTest{
 
         @Test
         public void findMaximumTest() {
-        	FindMaximum findMaximum = new FindMaximum(value1, value2, value3);
-            Assert.assertEquals(maxValue, findMaximum.findMaximum());
+        	FindMaximum findMaximum = new FindMaximum();
+            Assert.assertEquals(9, findMaximum.findMaximum(7, 9, 8, 5, 4));
+            Assert.assertEquals(35.8F, findMaximum.findMaximum(7f, 9f, 8.7f, 35.8f, 4f));
+            Assert.assertEquals("SitaFruit", findMaximum.findMaximum("RamFruit", "Peach", "SitaFruit", "Apple", "Banana"));
         }
 
-        @Parameterized.Parameters
-        public static Collection addValues() {
-                return Arrays.asList(new Object[][] {
-                        {9, 8, 7, 9},
-                        {8, 9, 7, 9},
-                        {8, 7, 9, 9},
-                        {7.9f, 5.4f, 4.3f, 7.9F},
-                        {5.4f, 7.9f, 4.3f, 7.9F},
-                        {4.3f, 5.4f, 7.9f, 7.9F},
-                        {"Peach", "Apple", "Banana", "Peach"},
-                        {"Apple", "Peach", "Banana", "Peach"},
-                        {"Apple", "Banana", "Peach", "Peach"}
-                });
-        }
 }
