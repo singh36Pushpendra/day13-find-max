b8bf29823dc8da24ab9b0abe2943036f50b4a6e4
singh36Pushpendra <psthakur.1999@gmail.com>
2022-11-06 21:03:32 +0530

    Added: TC2.3 Max float at 3rd pos

--- src/programs/generics/test/FindMaximumTest.java
+++ src/programs/generics/test/FindMaximumTest.java
@@ -9,6 +9,6 @@ public class FindMaximumTest {
 
 	@Test
 	public void findMaximumTest() {
-		Assert.assertEquals(7.9F, FindMaximum.findMaximum(5.2f, 7.9f, 7.44f));
+		Assert.assertEquals(7.9F, FindMaximum.findMaximum(5.2f, 7.44f, 7.9f));
 	}
 }
